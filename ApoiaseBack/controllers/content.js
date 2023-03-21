0e432c37dd9c434a28352f70a459fd4430cc2a56
rfragoso <raquel.fragoso@gmail.com>
2023-03-20 21:57:01 -0300

    Remove logs, organização e alteração do nome das variáveis

--- ApoiaseBack/controllers/content.js
+++ ApoiaseBack/controllers/content.js
@@ -3,95 +3,72 @@ const prisma = new PrismaClient()
 const moment = require('moment')
 
 function checkIfIsValidPublishDate(publishDate, actionMode){
-  console.log("publishDate:" + moment(publishDate))
   const diffMinutes = moment(publishDate).diff(moment(), "minutes")
-  console.log("diffMinutes:" + diffMinutes)
-  return diffMinutes >= 5 || actionMode == "postar-agora"
+  return diffMinutes >= 5 || actionMode == "post-now"
 }
-async function checkIfIsLessThanThree(actionMode)
-{
+async function checkIfIsLessThanThree(actionMode){
   date = moment(new Date() ).toDate()
   count = await prisma.content.count({
     where: {
         publishDate: { gt: date}
     }
     });
-    console.log(count);
-    
     return (count < 3);
-  
 }
 
 
 const create = async (req, res) => {
     let { title, body, publishDate, actionMode } = req.body;
-    console.log("publishDate: " + publishDate)
-    publishDate = moment(publishDate);
-    console.log("ActionMode: " + actionMode)
-    console.log("Titulo: " + title)
-    
+    publishDate = moment(publishDate);    
     
     const isValidPublishDate = checkIfIsValidPublishDate(publishDate, actionMode);
     const isLessThanThree = await checkIfIsLessThanThree(actionMode);
-    console.log("isValidDate: " + isValidPublishDate)
-    console.log("isLessThanThree: " + isLessThanThree)
-    
-    let errMsg = "";
+   
     if(!isValidPublishDate){
         return res.json({ error: "Por favor, selecione um horário pelo menos 5 minutos no futuro." });
     }
-    if(!isLessThanThree && actionMode == "postar-futuro"){
+    if(!isLessThanThree && actionMode == "post-future"){
       return res.json({ error: "É permitido agendar somente 3 postagens." });  
     }
     
-    
-        publishDate = publishDate.format();
-        const content = await prisma.content.create({
-            data: {
-                title,
-                body,
-                publishDate,
-            },
-        })
-        res.json(content)
+    publishDate = publishDate.format();
+    const content = await prisma.content.create({
+        data: {
+            title,
+            body,
+            publishDate,
+        },
+    })
+    res.json(content)
 }
 
 const edit = async (req, res) => {
     const { id } = req.params
     let { title, body, publishDate } = req.body;
     try {    
-        console.log("antes")
         const updatedContent = await prisma.content.update({
         where: { id },
         data: { title, body, publishDate: new Date(publishDate) },
         })
-        console.log("depois")
         res.json(updatedContent)
     } catch (error) {
-        //res.json({ error: error })
-        //res.status(404).json({ error: 'Content not found' })
         if (error.message === 'NOTFOUND') res.status(404).json({ error: 'content not found' })
     }
 }
 
 const getPending = async(req, res) => {
-  console.log("Date: " + new Date());
-  contents = await prisma.content.findMany(
-    {
-      where: 
-      {
+  contents = await prisma.content.findMany({
+      where: {
         publishDate: { gt: new Date() }
       }
     }
   )
-    res.send(contents)
+  res.send(contents)
 }
 
 const getPosted = async(req, res) => {
-  contents = await prisma.content.findMany(
-    {
-      where: 
-      {
+  contents = await prisma.content.findMany({
+      where: {
         publishDate: { lt: new Date() }
       }
     }
@@ -100,13 +77,13 @@ const getPosted = async(req, res) => {
 }
 
 const getUnique = async(req, res) => {
-    const { id } = req.params
-    content = await prisma.content.findUnique({
-      where: {
-        id: id,
-      },
-    })
-    res.send(content)
+  const { id } = req.params
+  content = await prisma.content.findUnique({
+    where: {
+      id: id,
+    },
+  })
+  res.send(content)
 }
 
 const deletePost = async (req, res) => {
@@ -118,8 +95,6 @@ const deletePost = async (req, res) => {
     })
     res.json(content)
 }
-  
-
 
 module.exports = {
     create, edit, getPending, getPosted, getUnique, deletePost
